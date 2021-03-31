1af9b21822967d91b8a1ff03dad673d5ec6377c2
AlexandraKapp <alexandra.kapp2@gmail.com>
2021-03-31 17:08:49 +0200

    add Augsburg

--- R/config.R
+++ R/config.R
@@ -1,13 +1,15 @@
-city_name <- "stuttgart"
+city_name <- "augsburg"
 delete_cache <- F
 
 pbf_file <- dplyr::case_when(
   # Stuttgart pbf file: https://download.geofabrik.de/europe/germany/baden-wuerttemberg/stuttgart-regbez.html
   city_name == "stuttgart" ~ "stuttgart-regbez-latest.osm.pbf",
   # Berlin pbf file: https://download.geofabrik.de/europe/germany/berlin.html
-  city_name == "xhain" ~ "berlin-latest.osm.pbf"
+  city_name == "xhain" ~ "berlin-latest.osm.pbf",
+  city_name == "augsburg" ~ "schwaben-latest.osm.pbf"
 )
 geo_selection_file <- dplyr::case_when(
   city_name == "stuttgart" ~ "stuttgart.geojson",
-  city_name == "xhain" ~ "xhain.geojson"
+  city_name == "xhain" ~ "xhain.geojson",
+  city_name == "augsburg" ~ "augsburg.geojson"
 )
